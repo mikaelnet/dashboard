38b421d52576e7de826f385465463cc15ab5dd7d
Mikael Högberg <mikael.hogberg@stendahls.se>
2015-01-22 22:56:24 +0100

    More stubs

---
+++ config.example.js
@@ -0,0 +1,10 @@
+module.exports = {
+  port: 3000,
+
+  twitter: { 
+  	consumer_key: 'your twitter consumer key',
+  	consumer_secret: 'your twitter secret key ',
+  	access_token_key: 'your access token key',
+  	access_token_secret: 'your access token key',
+  } 
+}
